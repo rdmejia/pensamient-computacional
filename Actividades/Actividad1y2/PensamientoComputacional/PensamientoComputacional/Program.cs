8c0a38fc7028aea6f1c585f9591c94004ccb8e6c
rdmejia <dmejia@healthcare.com>
2022-07-21 16:38:47 -0600

    Actividad1y2: Calcular el area de un circulo

---
+++ Actividades/Actividad1y2/PensamientoComputacional/PensamientoComputacional/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PensamientoComputacional
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            const double pi = Math.PI;
+
+            Console.Write("Ingrese el valor del radio de un círculo: ");
+            double r = double.Parse(Console.ReadLine());
+
+            double a = pi * r * r;
+
+            Console.WriteLine("El área del círculo es: {0}", a);
+        }
+    }
+}
