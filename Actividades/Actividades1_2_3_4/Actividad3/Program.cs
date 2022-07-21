2b8d0dc1b56db4ebcdd74894cd7833ad38c6c9d5
rdmejia <dmejia@healthcare.com>
2022-07-21 17:39:09 -0600

    Actividades: Agregar ejercicio 3 y 4

---
+++ Actividades/Actividades1_2_3_4/Actividad3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Actividad3
+{
+    class Program
+    {
+        static bool bigger(double x, double y)
+        {
+            return x > y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un número b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(bigger(a, b));
+            Console.WriteLine(bigger(1, 3));
+            Console.WriteLine(bigger(-10.8, -10.9));
+        }
+    }
+}
